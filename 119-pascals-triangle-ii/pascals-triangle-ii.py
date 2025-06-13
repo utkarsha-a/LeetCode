f5e5d7542222d25ad52a191d7e31b3c7ff2d1d5d
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-06-13 15:13:41 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (52.28%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+
+        for i in range(rowIndex):
+            row = [left+right for left, right in zip([0]+row, row+[0])]
+
+        return row
+        
+        
