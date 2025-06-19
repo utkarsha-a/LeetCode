6c1edc1051f4a7f3f47929f609b5cfa27ace1338
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-06-19 16:05:48 +0530

    Time: 12 ms (21.44%) | Memory: 19.3 MB (51.21%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        i = len(nums)
-        return nums[i//2]
+        hash = {}
 
+        for num in nums:
+            hash[num] = hash.get(num,0)+1
+            if hash[num] > len(nums) // 2:
+                return num
         
