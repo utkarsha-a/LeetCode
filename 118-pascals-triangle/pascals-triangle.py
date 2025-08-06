70d8d20d297074e137d344c1f5cdcb49b9c58874
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-08-06 12:44:35 +0530

    Time: 2 ms (11.88%) | Memory: 17.7 MB (87.08%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -3,10 +3,8 @@ class Solution:
         tri = []
         for i in range(numRows):
             row = [1]*(i+1)
-            for j in range(1,i):
-                row[j] = tri[i-1][j-1] + tri[i-1][j]
+            for j in range(1, i):
+              row[j] = tri[i-1][j-1] + tri[i-1][j]
             tri.append(row)
         return tri
 
-        
-        
