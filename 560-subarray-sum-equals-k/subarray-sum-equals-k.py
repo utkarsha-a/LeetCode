07703c3d18926dc674b88488aadfbbc5d8641d63
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-08-22 19:44:00 +0530

    Time: 27 ms (83.79%) | Memory: 20.4 MB (71.63%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,15 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        dic = defaultdict(int)
-        dic[0] = 1
-        runningSum = 0
-        count  = 0
-        for num in nums:
-            runningSum += num
-            difference = runningSum - k
-            if difference in dic:
-                count += dic[difference]
-            dic[runningSum] += 1
-        return count
+        hash = {0:1}
+        tot = cnt = 0
 
+        for n in nums:
+            tot += n
+
+            if tot-k in hash:
+                cnt += hash[tot-k]
+
+            hash[tot] = 1+hash.get(tot, 0)
+
+        return cnt
         
