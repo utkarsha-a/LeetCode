4eb4c06e65f4bd9d87c4e7fef61eba751a7fe378
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-08-24 10:54:58 +0530

    Time: 4 ms (63.21%) | Memory: 17.8 MB (39.90%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,17 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        prev = 0
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        res = 0
 
-        for chars in reversed(s):
-            curr = roman_values[chars]
-            if curr < prev:
-                total -= curr
+        for a, b in zip(s,s[1:]):
+            if roman[a] < roman[b]:
+                res -= roman[a]
             else:
-                total += curr
-            
-            prev = curr
-        
-        return total
+                res += roman[a]
+
+        return res+roman[s[-1]]
         
