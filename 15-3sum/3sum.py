547497288c2b8553227590b353f5b4849b31ac37
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-08-30 09:13:58 +0530

    Time: 613 ms (34.71%) | Memory: 20.7 MB (69.79%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,23 +4,31 @@ class Solution:
         nums.sort()
 
         for i in range(len(nums)):
-            if i>0 and nums[i] == nums[i-1]:
+            if i>0 and nums[i]==nums[i-1]:
                 continue
+            
+            left = i+1
+            right = len(nums)-1
+            
+            while left<right:
+                tot = nums[i] + nums[left] + nums[right]
 
-            j = i+1
-            k = len(nums)-1
+                if tot==0:
+                    res.append([nums[i], nums[left],nums[right]])
+                    left += 1
+                    right -= 1
 
-            while j<k:
-                total = nums[i] + nums[j] + nums[k]
-                if total>0:
-                    k-=1
-                elif total<0:
-                    j+=1
+                    while nums[left] == nums[left-1] and left<right:
+                        left += 1
+                    while nums[right]== nums[right+1] and left<right:
+                        right -= 1
+                elif tot>0:
+                    right -= 1
                 else:
-                    res.append([nums[i],nums[j],nums[k]])
-                    j+=1
-
-                    while nums[j] == nums[j-1] and j<k:
-                        j+=1
+                    left += 1
+                
         return res
+
+                
+
         
