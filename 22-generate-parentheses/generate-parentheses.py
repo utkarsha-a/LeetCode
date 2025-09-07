edb5bd88ca0060c6e6073d4f2b5e90aebb04e319
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-09-07 13:29:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.50%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -3,16 +3,17 @@ class Solution:
         res = []
 
         def dfs(openP, closeP, s):
-            if openP == closeP and openP + closeP == n * 2:
+            if openP == closeP and openP+closeP == n*2:
                 res.append(s)
                 return
             
-            if openP < n:
-                dfs(openP + 1, closeP, s + "(")
-            
-            if closeP < openP:
-                dfs(openP, closeP + 1, s + ")")
+            if openP<n:
+                dfs(openP+1, closeP, s+"(")
 
+            if closeP<openP:
+                dfs(openP, closeP+1, s+")")
+        
         dfs(0, 0, "")
 
         return res
+        
