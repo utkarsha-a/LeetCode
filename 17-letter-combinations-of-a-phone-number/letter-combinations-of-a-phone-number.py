3f62ddda707db539e128f27ccd02c6826a7e1ce0
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-09-13 19:56:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.56%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,28 +1,17 @@
-from typing import List
-
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []  
+        res = []
+        digitToChar = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7": "pqrs", "8":"tuv", "9":"wxyz"}
 
-        digit_to_letters = {
-            "2": "abc", "3": "def", "4": "ghi", 
-            "5": "jkl", "6": "mno", "7": "pqrs", 
-            "8": "tuv", "9": "wxyz"
-        }
+        def backtrack(i, curStr):
+            if len(digits)==len(curStr):
+                res.append(curStr)
+                return 
+            
+            for c in digitToChar[digits[i]]:
+                backtrack(i+1, curStr+c)
 
-        def backtrack(index, path):
-            if index == len(digits):
-                combinations.append("".join(path))
-                return
-
-            possible_letters = digit_to_letters[digits[index]]
-            for letter in possible_letters:
-                path.append(letter)  
-                backtrack(index + 1, path)  
-                path.pop() 
-        
-        combinations = []
-        backtrack(0, [])
-        return combinations
+        if digits:
+            backtrack(0, "")
+        return res
         
