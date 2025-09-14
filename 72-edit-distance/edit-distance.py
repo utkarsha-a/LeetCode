7584cbd2de98f8d63fb9f33a6470f6305fc4d297
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-09-14 19:56:29 +0530

    Time: 60 ms (65.75%) | Memory: 21.1 MB (56.88%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,26 +1,19 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        cache = {}
+        m = len(word1)
+        n = len(word2)
+        dp = [[float("inf")]*(n+1) for _ in range(m+1)]
 
-        def dp(i, j):
-            if (i, j) in cache:
-                return cache[(i, j)]
+        for j in range(n+1):
+            dp[m][j] =  n-j
+        for i in range(m+1):
+            dp[i][n] = m-i
+        
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if word1[i]==word2[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                else:
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j+1], dp[i+1][j+1])
 
-            if i == 0:
-                return j
-            if j == 0:
-                return i
-
-            if word1[i - 1] == word2[j - 1]:
-                res = dp(i - 1, j - 1)
-            else:
-                res = 1 + min(
-                    dp(i - 1, j),      # Delete
-                    dp(i, j - 1),      # Insert
-                    dp(i - 1, j - 1)   # Replace
-                )
-
-            cache[(i, j)] = res
-            return res
-
-        return dp(len(word1), len(word2))
+        return dp[0][0]
