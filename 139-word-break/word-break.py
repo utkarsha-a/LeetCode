21c393421e3e4380bf3906e8739dcee2ead58f87
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-09-15 01:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (87.10%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,17 +1,14 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        word_set = set(wordDict)  # Convert wordDict to a set for O(1) lookups
-        n = len(s)
-        dp = [False] * (n + 1)  # DP array to store results
-        dp[0] = True  # Empty string is always a valid segmentation
-        
-        # Iterate over all lengths of substrings
-        for i in range(1, n + 1):
-            for j in range(i):
-                # Check if s[j:i] is a word and dp[j] is True
-                if dp[j] and s[j:i] in word_set:
-                    dp[i] = True
+        dp = [False]*(len(s)+1)
+        dp[len(s)] = True
+
+        for i in range(len(s)-1,-1,-1):
+            for w in wordDict:
+                if (i+len(w)<=len(s)) and s[i:i+len(w)]==w:
+                    dp[i] = dp[i+len(w)]
+                if dp[i]:
                     break
-        
-        return dp[n]
+
+        return dp[0]
         
