cb3e51265b2512a201b64bb8c7737a1b12472555
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-09-22 22:34:19 +0530

    Time: 47 ms (69.39%) | Memory: 35.7 MB (8.07%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,6 +1,31 @@
 class Solution:
     @cache
     def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = []
+
+        def helper(i):
+            if i>=len(s):
+                res.append(part[:])
+                return
+            for j in range(i, len(s)):
+                if self.isPalindrome(s, i, j):
+                    part.append(s[i:j+1])
+                    helper(j+1)
+                    part.pop()
+
+        helper(0)
+        return res
+
+    def isPalindrome(self, s, l, r):
+        while l<r:
+            if s[l]!=s[r]:
+                return False
+            l+=1
+            r-=1
+        return True
+
+        '''
         if not s:
             return [[]]
         ans = []
@@ -9,4 +34,4 @@ class Solution:
                 for suf in self.partition(s[i:]):
                     ans.append([s[:i]] + suf)
         return ans
-        
+        '''
