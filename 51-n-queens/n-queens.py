078cfa7fc355abb397c3079c1544f1b394c64886
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-09-23 20:12:57 +0530

    Time: 11 ms (72.49%) | Memory: 18.5 MB (16.27%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posD = set()
+        negD = set()
+        board = [["."]*n for i in range(n)]
+        res = []
+
+        def backtrack(r):
+            if r==n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+
+            for c in range(n):
+                if c in col or (r+c) in posD or (r-c) in negD:
+                    continue
+                
+                col.add(c)
+                posD.add(r+c)
+                negD.add(r-c)
+                board[r][c] = "Q"
+
+                backtrack(r+1)
+
+                col.remove(c)
+                posD.remove(r+c)
+                negD.remove(r-c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return res
+        
