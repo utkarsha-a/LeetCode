fca5c855a64ab2b27e7065fee23dc59719fbec1c
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2025-12-14 11:38:29 +0530

    Time: 44 ms (65.98%) | Memory: 32.3 MB (40.46%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,35 +1,65 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
 class Trie:
 
     def __init__(self):
+        self.root = TrieNode()
+        '''
         self.trie = {}
-        
+        '''
+
 
     def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+        '''
         d = self.trie
         for c in word:
-            if c not in d:
+            if c not in trie:
                 d[c] = {}
             d = d[c]
         d['.'] = '.'
-
-        
-
+        '''
+      
     def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+        '''
         d = self.trie
         for c in word:
             if c not in d:
                 return False
             d = d[c]
         return '.' in d
+        '''
         
 
     def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+        '''
         d = self.trie
         for c in prefix:
             if c not in d:
                 return False
             d = d[c]
         return True
+        '''
         
 
 
