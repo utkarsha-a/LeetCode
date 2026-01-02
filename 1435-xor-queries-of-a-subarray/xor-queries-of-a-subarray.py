739387083acd99fc1097644a8419f1ed3342528c
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-01-02 14:56:59 +0530

    Time: 13 ms (95.64%) | Memory: 33.9 MB (5.30%) - LeetSync

---
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        n = len(arr)
+        pre = [0]*n
+        pre[0] = arr[0]
+        for i in range(1, n):
+            pre[i] = pre[i-1] ^ arr[i]
+
+        ans = []
+        for l, r in queries:
+            if l==0:
+                ans.append(pre[r])
+            else:
+                ans.append(pre[r] ^ pre[l-1])
+
+        return ans        
