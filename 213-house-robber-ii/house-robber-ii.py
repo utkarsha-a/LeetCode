59d8d2a1da52cab61c0014c6a123a81064a223f1
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-01-14 00:00:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.69%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def maxmoney(nums):
+            prev = 0
+            maxx = 0
+
+            for cur in nums:
+                temp = max(maxx, prev+cur)
+                prev = maxx
+                maxx = temp
+
+            return maxx
+
+        return max(maxmoney(nums[:-1]), maxmoney(nums[1:]), nums[0])
+        
