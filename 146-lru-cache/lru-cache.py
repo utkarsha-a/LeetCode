fd1c586aa78c9b79729e7651d1092be8066499b9
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-01-15 13:48:23 +0530

    Time: 136 ms (54.92%) | Memory: 79.1 MB (6.49%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,16 +1,22 @@
+'''
+deque: pop and apppend take O(1), cannot use deque, as accesing randomn element will take O(n)
+DLL + hashmap: correct
+DLL - we can accesss element in O(n), so we use hashmap to access element in O(1)
+'''
 class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
         self.prev = None
         self.next = None
-class LRUCache:
 
+class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
         self.oldest = Node(0,0)
         self.latest = Node(0,0)
+        self.oldest.next = self.latest
         self.latest.prev = self.oldest        
 
     def get(self, key: int) -> int:
