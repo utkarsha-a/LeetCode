c442fba9d9486bcab01efa4214b5b4c666b4deac
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-01-15 18:30:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (9.45%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -3,13 +3,10 @@ class Solution:
         def maxmoney(nums):
             prev = 0
             maxx = 0
-
             for cur in nums:
                 temp = max(maxx, prev+cur)
                 prev = maxx
                 maxx = temp
-
             return maxx
-
-        return max(maxmoney(nums[:-1]), maxmoney(nums[1:]), nums[0])
+        return max(maxmoney(nums[1:]), maxmoney(nums[:-1]), nums[0])
         
