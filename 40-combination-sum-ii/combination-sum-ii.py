8c59f3fd4c0391045dc9a4b713353b38f7a284d8
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-01-25 15:48:44 +0530

    Time: 31 ms (24.45%) | Memory: 19.7 MB (15.52%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,23 +1,24 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        sub = []
         n = len(candidates)
         candidates.sort()
 
-        def helper(idx, cur, tot):
+        def helper(idx, tot):
             if tot==target:
-                res.append(cur[:])
-                return
+                res.append(sub[:])
+                return 
             if idx>=n or tot>target:
                 return 
-            
-            cur.append(candidates[idx])
-            helper(idx+1, cur, tot+candidates[idx])
-            cur.pop()
 
+            sub.append(candidates[idx])
+            helper(idx+1, tot+candidates[idx])
+            
+            sub.pop()
             while idx+1<n and candidates[idx]==candidates[idx+1]:
                 idx+=1
-            helper(idx+1, cur, tot)
+            helper(idx+1, tot)
 
-        helper(0, [], 0)
+        helper(0, 0)
         return res
