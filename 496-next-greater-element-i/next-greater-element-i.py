10c874664d16da10f6daf1e7f66080290799bee1
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-01-31 18:26:34 +0530

    Time: 1 ms (50.38%) | Memory: 19.5 MB (14.86%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hashmp = {n:i for i, n in enumerate(nums1)}
+        res = [-1]*len(nums1)
+
+        for i in range(len(nums2)):
+            if nums2[i] not in hashmp:
+                continue
+            for j in range(i, len(nums2)):
+                if nums2[j]>nums2[i]:
+                    idx = hashmp[nums2[i]]
+                    res[idx] = nums2[j]
+                    break
+        
+        return res
+
