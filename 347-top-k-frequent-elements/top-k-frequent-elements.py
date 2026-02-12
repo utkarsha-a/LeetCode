b568718779cd83edfd494764ea8ee98fbe9c70c6
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-02-12 23:40:17 +0530

    Time: 10 ms (27.97%) | Memory: 24.9 MB (13.25%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,5 +1,19 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #O(n), O(n)
+        hashmp = {}
+        for n in nums:
+            hashmp[n] = hashmp.get(n, 0) + 1
+        freq = [[] for _ in range(len(nums)+1)]
+        for n, f in hashmp.items():
+            freq[f].append(n)
+        res = []
+        for i in range(len(freq)-1, -1, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res)==k:
+                    return res
+        '''
         # O(nlogk), O(n)
         heap = []
         hashmp = {}
@@ -14,4 +28,4 @@ class Solution:
             res.append(heapq.heappop(heap)[1])
 
         return res 
-        
+        '''
