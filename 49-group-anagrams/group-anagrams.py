537eb767380a6ec5a3c44603f4e064680b115d3a
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-02-12 10:37:04 +0530

    Time: 13 ms (55.03%) | Memory: 22.2 MB (47.25%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #O(mnlogn), O(mn)
+        ans = defaultdict(list)
+
+        for s in strs:
+            key = "".join(sorted(s))
+            ans[key].append(s)
+        
+        return list(ans.values())
