8778bf7b34a4401101639801748378b992132bcd
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-04-10 21:25:01 +0530

    Time: 63 ms (75.96%) | Memory: 19.7 MB (51.01%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,35 +1,40 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
+        if t=="":
             return ""
-        
-        char_count = defaultdict(int)
-        for ch in t:
-            char_count[ch] += 1
-        
-        target_chars_remaining = len(t)
-        min_window = (0, float("inf"))
-        start_index = 0
 
-        for end_index, ch in enumerate(s):
-            if char_count[ch] > 0:
-                target_chars_remaining -= 1
-            char_count[ch] -= 1
+        countT = {}
+        window = {}
 
-            if target_chars_remaining == 0:
-                while True:
-                    char_at_start = s[start_index]
-                    if char_count[char_at_start] == 0:
-                        break
-                    char_count[char_at_start] += 1
-                    start_index += 1
-                
-                if end_index - start_index < min_window[1] - min_window[0]:
-                    min_window = (start_index, end_index)
-                
-                char_count[s[start_index]] += 1
-                target_chars_remaining += 1
-                start_index += 1
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have = 0
+        need = len(countT)
+
+        res = [-1, -1]
+        resLen = float("infinity")
+
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c]==countT[c]:
+                have += 1
+
+            while have==need:
+                if (r-l+1) < resLen:
+                    res = [l, r]                                         #update res ans resLen
+                    resLen = (r-l+1)
+
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:   #pop left of the window, shorten it
+                    have -= 1
+                l += 1
         
-        return "" if min_window[1] > len(s) else s[min_window[0]:min_window[1]+1]
-        
+        l, r = res
+        if resLen != float("infinity"):
+            return s[l:r+1]
+        else:
+            return ""
