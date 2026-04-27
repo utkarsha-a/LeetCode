6c9f74487f48262c0c74b521763282370a3fffa4
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-04-27 22:55:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (79.79%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,20 +1,29 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        r = len(matrix)
-        c= len(matrix[0])
-        l = 0
-        rig = r*c-1
+        rows, cols = len(matrix), len(matrix[0])
+        
+        top, bot = 0, rows-1
 
-        while l<=rig:
-            mid = (l+rig)//2
-            row, col = mid//c, mid % c
-            guess = matrix[row][col]
+        while top<=bot:
+            mid_row = (top+bot)//2
+            if target > matrix[mid_row][-1]:
+                top = mid_row + 1
+            elif target < matrix[mid_row][0]:
+                bot = mid_row - 1
+            else:
+                break
 
-            if guess == target:
-                return True
-            elif guess < target:
+        if not (top<=bot):
+            return False
+        mid_row = (top+bot)//2
+        l, r = 0, cols-1
+        while l<=r:
+            mid = (l+r)//2
+            if target > matrix[mid_row][mid]:
                 l = mid+1
-            else: 
-                rig = mid-1
+            elif target < matrix[mid_row][mid]:
+                r = mid-1
+            else:
+                return True
 
         return False
