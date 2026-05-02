e671f98931bf5621c468dfaf63ae50197ae90a12
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-05-02 18:42:23 +0530

    Time: 148 ms (24.75%) | Memory: 78.9 MB (7.22%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,8 +1,3 @@
-'''
-deque: pop and apppend take O(1), cannot use deque, as accesing randomn element will take O(n)
-DLL + hashmap: correct
-DLL - we can accesss element in O(n), so we use hashmap to access element in O(1)
-'''
 class Node:
     def __init__(self, key, val):
         self.key = key
@@ -19,16 +14,16 @@ class LRUCache:
         self.latest = Node(0,0)
         self.oldest.next = self.latest
         self.latest.prev = self.oldest
-        
+
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-    
+
     def remove(self, node):
-        prev = node.prev
+        prev = node.prev 
         next = node.next
         prev.next = next
         next.prev = prev
@@ -39,7 +34,7 @@ class LRUCache:
         prev.next = node
         next.prev = node
         node.prev = prev
-        node.next = next        
+        node.next = next       
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
@@ -51,6 +46,9 @@ class LRUCache:
             lru = self.oldest.next
             self.remove(lru)
             del self.cache[lru.key]
+        
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
