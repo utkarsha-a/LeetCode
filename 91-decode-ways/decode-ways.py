a595358d7d9456d58bafa5ca8cdbbfabf77deb41
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-06-02 13:11:44 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (40.28%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,5 +1,21 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        #DP SOLN
+        dp = {len(s):1}
+
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+            
+            if (i+1 < len(s) and (s[i]=="1" or s[i]=="2" and s[i+1] in "0123456")):
+                dp[i] += dp[i+2]
+            
+        return dp[0]
+
+        '''
+        #RECURSIVE SOLN
         dp = {len(s):1}
 
         def dfs(i):
@@ -15,3 +31,4 @@ class Solution:
             return res
         
         return dfs(0)
+        '''
