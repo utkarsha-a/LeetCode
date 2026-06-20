588543025ac221ea094c768af31564f5a836d456
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-06-20 09:47:01 +0530

    Time: 3 ms (95.47%) | Memory: 20.3 MB (43.36%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,7 +1,25 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        #DP SOLUTION - O(n)
+        dp = [0] * (n+1)
+        offset = 1
+
+        for i in range(1, n+1):
+            if offset*2 == i:
+                offset = i
+            dp[i] = 1 + dp[i-offset]
+        
+        return dp
+        
+        '''
+        O(nlogn)
         ans = [0] * (n+1)
         for i in range(1, n+1):
-            ans[i] = ans[i >> 1] + (i & 1)
+            num = i
+            res = 0
+            while num!=0:
+                res += 1
+                num = num & (num-1)
+            ans[i] = res
         return ans
-        
+        '''
