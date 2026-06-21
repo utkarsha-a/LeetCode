4eebcb9c1394625bb4cbcd969e0faa8c5a0bd57c
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-06-21 17:21:46 +0530

    Time: 4 ms (4.30%) | Memory: 19.3 MB (32.91%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -9,14 +9,14 @@ class Solution:
         cur = dummy
 
         while l1 and l2:
-            if l1.val>l2.val:
-                cur.next = l2
-                l2 = l2.next
+            if l1.val<=l2.val:
+                cur.next= l1
                 cur = cur.next
-            elif l2.val>=l1.val:
-                cur.next = l1
                 l1 = l1.next
+            elif l2.val<l1.val:
+                cur.next = l2
                 cur = cur.next
+                l2 = l2.next
             
         if l1:
             cur.next = l1
@@ -24,9 +24,8 @@ class Solution:
         if l2:
             cur.next = l2
             cur = cur.next
-        
-        return dummy.next
 
+        return dummy.next       
 
 
         
