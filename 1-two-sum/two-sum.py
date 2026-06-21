947f9194aac05325027e99dc336266fe16c08700
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-06-21 17:07:29 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (41.68%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,5 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmp = {}
+        for i in range(len(nums)):
+            if target-nums[i] in hashmp:
+                return [i, hashmp[target-nums[i]]]
+            hashmp[nums[i]] = i
+        return False
+
+
+
+'''
+
         mp = {}
         for i in range(len(nums)):
             if target-nums[i] in mp:
@@ -7,3 +18,4 @@ class Solution:
             mp[nums[i]] = i
         
         return False
+'''
