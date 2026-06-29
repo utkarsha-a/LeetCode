222510fb77adff8fa6b78fd9acddcc6b60dc1788
utkarsha-a <139202919+utkarsha-a@users.noreply.github.com>
2026-06-29 15:09:46 +0530

    Time: 105 ms (5.11%) | Memory: 34 MB (11.97%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,5 +1,24 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        i, j = 0, len(nums)-1
+        i2, j2 = 0, len(nums)-1
+        res = [0]*len(nums)
+
+        while i<len(nums) and j>=0:
+            if nums[i]<pivot:
+                res[i2] = nums[i]
+                i2 += 1
+            if nums[j]>pivot:
+                res[j2] = nums[j]
+                j2 -= 1
+            i, j = i+1, j-1
+        
+        while i2<=j2:
+            res[i2] = res[j2] = pivot
+            i2, j2 = i2+1, j2-1
+        return res
+        '''
+        # O(N), O(N)
         small, big, num = [], [], []
 
         for n in nums:
@@ -11,3 +30,4 @@ class Solution:
                 num.append(n)
         
         return small+num+big
+        '''
